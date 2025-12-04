a1c69fa3bd3ce6516d281499f24618faef6c1d7e
Vladimir Sukanica <vladimir.sukanica@zesium.com>
2025-12-04 12:38:31 +0100

    Add app folders

---
+++ Apps/CiCdTesting/CiCdTesting/CiCdTesting/CiCdTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CiCdTestingApp.swift
+//  CiCdTesting
+//
+//  Created by New on 4.12.25..
+//
+
+import SwiftUI
+
+@main
+struct CiCdTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apps/CiCdTestingDev/CiCdTestingDev/CiCdTestingDev/CiCdTestingDevApp.swift
@@ -0,0 +1,17 @@
+//
+//  CiCdTestingDevApp.swift
+//  CiCdTestingDev
+//
+//  Created by New on 4.12.25..
+//
+
+import SwiftUI
+
+@main
+struct CiCdTestingDevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
