883b954a597716ffbea5db162e23d612a2d168e8
Vladimir Sukanica <vladimir.sukanica@zesium.com>
2025-12-05 13:33:34 +0100

    Testin 1

--- Apps/CiCdTesting/CiCdTesting/CiCdTesting/CiCdTesting/ContentView.swift
+++ Apps/CiCdTesting/CiCdTesting/CiCdTesting/CiCdTesting/ContentView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    var text: String = "Testing"
+    var text: String = "Testin"
     var body: some View {
         VStack {
             Image(systemName: "globe")
