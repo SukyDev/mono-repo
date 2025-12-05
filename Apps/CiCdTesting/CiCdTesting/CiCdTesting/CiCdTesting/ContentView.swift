2e07f77f58b0780aebe613bf2db75716c363231a
Vladimir Sukanica <vladimir.sukanica@zesium.com>
2025-12-05 11:23:09 +0100

    TPM 8

--- Apps/CiCdTesting/CiCdTesting/CiCdTesting/CiCdTesting/ContentView.swift
+++ Apps/CiCdTesting/CiCdTesting/CiCdTesting/CiCdTesting/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Testing push to main 7!")
+            Text("Testing push to main 8!")
         }
         .padding()
     }
