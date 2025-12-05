4f28c37a280bcef9110d967bb86305fc8cee484a
Vladimir Sukanica <vladimir.sukanica@zesium.com>
2025-12-05 08:46:23 +0100

    Initial setup for projects

---
+++ Apps/CiCdTesting/CiCdTesting/CiCdTesting/CiCdTesting/cicd-ios/Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "cicd-ios",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "cicd-ios",
+            targets: ["cicd-ios"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "cicd-ios"),
+        .testTarget(
+            name: "cicd-iosTests",
+            dependencies: ["cicd-ios"]
+        ),
+    ]
+)
