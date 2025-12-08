01daaedeef948fc73db14444e0637d2ab412a315
Vladimir Sukanica <vladimir.sukanica@zesium.com>
2025-12-08 10:55:03 +0100

    Test flow with failed test

--- Apps/CiCdTestingDev/CiCdTestingDev/CiCdTestingDev/CiCdTestingDev/ContentView.swift
+++ Apps/CiCdTestingDev/CiCdTestingDev/CiCdTestingDev/CiCdTestingDev/ContentView.swift
@@ -8,12 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    var text: String = "Test text"
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(text)
         }
         .padding()
     }
