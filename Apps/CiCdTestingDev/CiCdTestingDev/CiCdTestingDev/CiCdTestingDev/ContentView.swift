7dd7f1c818e65d87145a60bff2c0a1d595ec3e62
Vladimir Sukanica <vladimir.sukanica@zesium.com>
2025-12-08 11:17:19 +0100

    Test failing on right simulator

--- Apps/CiCdTestingDev/CiCdTestingDev/CiCdTestingDev/CiCdTestingDev/ContentView.swift
+++ Apps/CiCdTestingDev/CiCdTestingDev/CiCdTestingDev/CiCdTestingDev/ContentView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    var text: String = "Test text"
+    var text: String = "Test tex"
     var body: some View {
         VStack {
             Image(systemName: "globe")
