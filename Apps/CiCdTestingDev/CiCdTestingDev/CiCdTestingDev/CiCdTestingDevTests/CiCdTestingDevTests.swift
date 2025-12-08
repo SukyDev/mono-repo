276131a2dc6244b2b6fbcda492b019722dde28bc
Vladimir Sukanica <vladimir.sukanica@zesium.com>
2025-12-08 11:08:49 +0100

    Test with success tests

--- Apps/CiCdTestingDev/CiCdTestingDev/CiCdTestingDev/CiCdTestingDevTests/CiCdTestingDevTests.swift
+++ Apps/CiCdTestingDev/CiCdTestingDev/CiCdTestingDev/CiCdTestingDevTests/CiCdTestingDevTests.swift
@@ -20,7 +20,7 @@ final class CiCdTestingDevTests: XCTestCase {
 
     func testExample() throws {
         let contentView = ContentView()
-        XCTAssertTrue(contentView.text == "Testinga")
+        XCTAssertTrue(contentView.text == "Test text")
     }
 
     func testPerformanceExample() throws {
